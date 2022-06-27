39a3a7c831db89c15de4be492939679ac722253c
RenanValentim <renanevalentim93@gmail.com>
2022-06-27 10:23:36 -0300

    first commit

---
+++ src/screens/CarDetails/styles.tsx
@@ -0,0 +1,13 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import { FlatList } from "react-native";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
+
+import { Container, Header, HeaderContent, TotalCars, CarList } from "./styles";
+
+export function Home() {
+  const carData = {
+    brand: "AUDI",
+    name: "RS 5 coupé",
+    rent: {
+      period: "AO DIA",
+      price: 120,
+    },
+    thumbnail: "https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png",
+  };
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7, 8]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      ></CarList>
+    </Container>
+  );
+}
